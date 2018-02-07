7f7abb4cc41ddeb80cbec832738a31f7b84f74cb
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2018-02-07 22:27:40 +0100

    Window state test bed.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 // Example template
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Example");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Example", sf::Style::Resize);
 
+    bool isHidden = true;
     // run the program as long as the window is open
     while (window.isOpen())
     {
@@ -20,6 +22,28 @@ int main()
                 // close if escape key was pressed
                 if (event.key.code == sf::Keyboard::Escape) {
                     window.close();
+                }
+                else if (event.key.code == sf::Keyboard::M) {
+                    window.setState(sf::State::Minimized);
+                    sf::sleep(sf::seconds(3));
+                    std::cout << window.getState() << std::endl;
+                    window.setState(sf::State::Maximized);
+                }
+                else if (event.key.code == sf::Keyboard::N) {
+                    window.setState(sf::State::Maximized);
+                }
+                else if (event.key.code == sf::Keyboard::W) {
+                    window.setState(sf::State::Windowed);
+                }
+                else if (event.key.code == sf::Keyboard::F) {
+                    window.setState(sf::State::Fullscreen);
+                }
+                else if (event.key.code == sf::Keyboard::H) {
+                    isHidden = !isHidden;
+                    window.setVisible(isHidden);
+                }
+                else if (event.key.code == sf::Keyboard::G) {
+                    std::cout << window.getState() << std::endl;
                 }
             }
         }
